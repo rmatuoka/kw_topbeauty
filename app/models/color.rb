ad98b5849b09fa14be066810f8bd56de59f1bf2e
Cristiano Nascimento <cnascimento@korewa.com.br>
2012-03-15 15:34:54 -0700

    produtos

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,6 +1,6 @@
 class Color < ActiveRecord::Base
   belongs_to :product
-  has_attached_file :imgpallete,:styles => { :medium => "200x286>", :thumb => "100x100>", :small => "50x50>" }
+  has_attached_file :imgpallete,:styles => { :medium => "200x286>", :thumb => "100x100>", :small => "19x19>" }
   has_attached_file :imgenamel,:styles => { :medium => "200x286>",:smallhome => "74x202", :thumb => "100x100>" , :small => "50x50>"}
   has_attached_file :imghand,:styles => { :medium => "200x286>", :thumb => "100x100>", :small => "50x50>" }
 end
