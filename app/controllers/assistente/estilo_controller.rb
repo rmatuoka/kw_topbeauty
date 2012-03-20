b27780da04d054812e980aa3e6e2202c82884029
Rafael Matuoka <rmatuoka@korewa.com.br>
2012-03-19 22:09:26 -0300

    ajustes asistente + página de produtos

--- app/controllers/assistente/estilo_controller.rb
+++ app/controllers/assistente/estilo_controller.rb
@@ -116,18 +116,22 @@ class Assistente::EstiloController < ApplicationController
     end
     
     if @isTimida
+      @tweet = "Eu sou Tímida! Faça você também o teste! http://www.topbeauty.com.br"
       @titulo = "Você é Tímida!"
       @chamada = "Fica na sua"
       @texto = "Olá, agora que eu já conheço um pouco mais sobre você, posso dizer que seu perfil é de uma pessoa Tímida. Você está sempre na sua, não gosta de contato com grandes multidões, é discreta e adora estar entre amigos mais próximos. Os esmaltes que combinam com você são: "
     elsif @isExtrovertida
+      @tweet = "Eu sou Extrovertida! Faça você também o teste! http://www.topbeauty.com.br"
       @titulo = "Você é Extrovertida!"
       @chamada = "Luz, holofotes, animação!"
       @texto = "Sempre com os amigos, é quem agita a galera pra sair. Comunicativa e animada você é sempre destaque. Por ser o centro das atenções, os esmaltes que mais empolgam você são:"
     elsif @isRomantica
+      @tweet = "Eu sou Romântica! Faça você também o teste! http://www.topbeauty.com.br"
       @titulo = "Você é Romântica!"
       @chamada = "Ahhhhh, o amor!"
       @texto = "Para você o que tem valor na vida são as pequenas coisas, os gestos de carinho e os sentimentos verdadeiros. Sempre em busca do príncipe encantado, idealiza o homem perfeito, que faz de tudo para agradar sua parceira. Seu perfil Romântico pede esmaltes assim: "
     elsif @isOusada
+      @tweet = "Eu sou Ousada! Faça você também o teste! http://www.topbeauty.com.br"
       @titulo = "Você é Ousada!"
       @chamada = "Tô na área!"
       @texto = "Você não tem medo de correr riscos, é ágil, prática e corajosa ao resolver qualquer situação que possa surgir em seu caminho. Nos relacionamentos, o que vale é arriscar, provocar e inovar. Seu perfil é de garota Ousada e no quesito esmaltes, estes são a sua cara: "
