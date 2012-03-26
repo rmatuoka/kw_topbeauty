630e8febd12c27475f0035e2ef30d883cdfe4cdb
Cristiano Nascimento <cnascimento@korewa.com.br>
2012-03-26 07:20:04 -0700

    alterações mailers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,12 +3,12 @@ class UserMailer < ActionMailer::Base
   
   def contact (contato)
       @contact= contato
-      mail(:to => "cnascimento@korewa.com.br",:bcc => "cnascimento@korewa.com.br", :subject => "teste top beauty")	
+      mail(:to => "sac@topbeauty.com.br",:bcc => "sac@topbeauty.com.br", :subject => "Contato via Site")	
   end
   
   def t_conosco(contato)
       @contact= contato 
 	  attachments[@contact.curriculum_file_name] = File.read(@contact.curriculum.path) 
-      mail(:to => "cnascimento@korewa.com.br",:bcc => "cnascimento@korewa.com.br", :subject => "teste top beauty")
+      mail(:to => "sac@topbeauty.com.br",:bcc => "sac@topbeauty.com.br", :subject => "Contato via Site")
   end	
 end
