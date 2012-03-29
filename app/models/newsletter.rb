73e7ae3a5300849942f029a0791a83b1a802f17e
Cristiano Nascimento <cnascimento@korewa.com.br>
2012-03-29 13:40:41 -0700

    Ajuste Model

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,3 +1,5 @@
 class Newsletter < ActiveRecord::Base
-  attr_accessible :email
+  #attr_accessible :email
+  validates_presence_of :email
+  validates_format_of :email,:with => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i
 end
