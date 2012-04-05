800bf99fd676f0c2f2fddcfc327c8f4443a21dff
-_- Zaba -_- <bsilva@korewa.com.br>
2012-04-05 16:45:40 -0300

    Ajuste Log Contato

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,12 +3,12 @@ class UserMailer < ActionMailer::Base
   
   def contact (contato)
       @contact= contato
-      mail(:to => "sac@topbeauty.com.br",:bcc => "sac@topbeauty.com.br", :subject => "Contato via Site",:reply_to => contato.email)	
+      mail(:to => "sac@topbeauty.com.br",:bcc => "log@korewa.com.br", :subject => "Contato via Site",:reply_to => contato.email)	
   end
   
   def t_conosco(contato)
       @contact= contato 
 	  attachments[@contact.curriculum_file_name] = File.read(@contact.curriculum.path) 
-      mail(:to => "sac@topbeauty.com.br",:bcc => "sac@topbeauty.com.br", :subject => "Currículo",:reply_to => contato.email)
+      mail(:to => "sac@topbeauty.com.br",:bcc => "log@korewa.com.br", :subject => "Currículo",:reply_to => contato.email)
   end	
 end
